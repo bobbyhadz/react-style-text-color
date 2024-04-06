d9cd9117d3f43e041d894ce2cecf5d150816f3e9
bobbyhadz <opmftw@gmail.com>
2024-04-06 14:37:03 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <p style={{fontSize: '2rem'}}>
+        Some{' '}
+        <span style={{color: 'white', backgroundColor: 'lime'}}>
+          colorful
+        </span>{' '}
+        text
+      </p>
+    </div>
+  );
+};
+
+export default App;
